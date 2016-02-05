f3da3bf9c869f64f9c32659aa8060f7d8e80009f
Edmund Widl <Edmund.Widl@ait.ac.at>
2016-02-05 13:58:02 +0100

    minor changes for submitting successfully to PyPI

--- setup.py
+++ setup.py
@@ -2,40 +2,41 @@ from distutils.core import setup
 
 
 # List of additional files (i.e., files without the '.py' extension) that are part of the distribution.
-additional_files = [
+pyfmipp_additional_files = [
   'lib/_fmippim.pyd',
   'lib/fmippim.dll',
   'lib/sundials_cvode.dll',
   'lib/sundials_nvecserial.dll',
   'licenses/FMIPP_LICENSE.txt',
-  'licenses/SUNDIALS_LICENSE.txt'
+  'licenses/SUNDIALS_LICENSE.txt',
   ]
 
   
 # Read long description from file (reStructuredText syntax). Will be parsed and displayed as HTML online.
-with open( 'README.txt' ) as file: long_description = file.read()
+with open( 'README.txt' ) as file: pyfmipp_long_description = file.read()
 
 
 # Specify the setup of this package.
 setup(
   name = 'fmipp',
-  version = '0.1',
+  version = '0.3',
   description = 'FMI++ Python Interface for Windows',
   #long_description = 'This package provides a Python wrapper for the FMI++ library, which \nintends to bridge the gap between the basic fuctionality provided by \nthe FMI specification and the typical requirements of simulation tools.',
-  long_description = long_description,
+  long_description = pyfmipp_long_description,
   url = 'http://fmipp.sourceforge.net',
   maintainer = 'Edmund Widl',
   maintainer_email = 'edmund.widl@ait.ac.at',
   license = 'BSD license & BOOST software license',
   platforms = 'Windows',
+  keywords = [ 'FMI', 'Functional Mock-up Interface', 'FMI++ Library' ],
   classifiers=[
     'Development Status :: 4 - Beta',
-	'Intended Audience :: End Users/Desktop',
 	'Intended Audience :: Science/Research',
 	'Operating System :: Microsoft :: Windows',
 	'Topic :: Scientific/Engineering',
-	'Programming Language :: Python',
+	'Programming Language :: Python :: 2.7',
+	'Programming Language :: C++',
 	],
   packages = [ 'fmipp' ],
-  package_data = { 'fmipp': additional_files },
+  package_data = { 'fmipp': pyfmipp_additional_files },
   )
--- upload.bat
+++ upload.bat
@@ -4,7 +4,7 @@ REM Set environment variable HOME to the directory containing file '.pypirc'
 SET HOME=%~DP0
 
 REM Register.
-python setup.py register
+python.exe "%~DP0\setup.py" --no-target-compile --no-target-optimize --title "FMI++ Python Interface for Windows" --bitmap logo\logo_py_fmipp.bmp register
 
 REM Upload.
-python setup.py bdist_wininst upload
+python.exe "%~DP0\setup.py" bdist_wininst --no-target-compile --no-target-optimize --title "FMI++ Python Interface for Windows" --bitmap logo\logo_py_fmipp.bmp upload
