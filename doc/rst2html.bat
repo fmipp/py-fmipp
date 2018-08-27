96250e48a53bb1bec1aa71dc189027acffaaa9fb
Edmund Widl <edmund.widl@ait.ac.at>
2018-08-27 14:06:01 +0200

    minor update to docs

--- doc/rst2html.bat
+++ doc/rst2html.bat
@@ -1,1 +1,5 @@
-python.exe D:\Python27\Scripts\rst2html.py --stylesheet=pygments-long.css,style.css doc.txt index.html
+REM To run this script, install 'doctutils' and 'pygments' for Python first!
+
+SET PYTHON_DIR=C:\Python37-x64
+
+%PYTHON_DIR%\python.exe %PYTHON_DIR%\Scripts\rst2html.py --stylesheet=pygments-long.css,style.css doc.txt index.html
